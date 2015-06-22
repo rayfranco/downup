c8a37f381a6de38b5bc7ec27894e81d4566665c0
Franco Bouly <fbouly@gmail.com>
2015-06-22 23:36:20 +0200

    Fix scroll memory

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 var TinyEmitter = require('tiny-emitter');
 
 var Downup = module.exports = function(init) {
+  this.updating = false;
   this.value = init || 0;
+  this.test = init || 0;
 };
 
 Downup.prototype = Object.create(TinyEmitter.prototype);
@@ -13,14 +15,35 @@ Downup.prototype.set = function(value) {
 };
 
 Downup.prototype.update = function(value) {
-  var offset = value - this.value,
-      min = Math.min(value, this.value),
-      max = Math.max(value, this.value);
+  this.test = value;
+
+  if (this.updating && value === this.value) return;
+
+  this.updating = true;
+
+  if (requestAnimationFrame)
+    requestAnimationFrame(this._update.bind(this)); // Browser
+  else
+    this._update(); // Node
+};
+
+// Internal update
+Downup.prototype._update = function(){
+  var offset = this.test - this.value,
+      min = Math.min(this.test, this.value),
+      max = Math.max(this.test, this.value);
 
   for (var evt in this.e) {
     evt = Number(evt);
-    if (evt > min && evt < max) this.emit(evt, offset);
+    if (
+      (evt > min && evt < max) ||
+      (evt === min && offset < 0) ||
+      (evt === max && offset > 0)
+    ) {
+      this.emit(evt, offset);
+    }
   }
 
-  this.value = value;
-};
+  this.value = this.test;
+  this.updating = false;
+}
